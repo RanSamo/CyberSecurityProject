45991f7cf8ab4d51ce8ac3bd918d6f6245c55640
daniel karminsky <129665077+danielkainsky@users.noreply.github.com>
2025-04-28 15:21:50 +0300

    Create security-utils.js

---
+++ Cyber-Server/auth/security-utils.js
@@ -0,0 +1,8 @@
+function generateTemporaryPassword(length = 8) {
+    const chars = 'abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789';
+    let tempPassword = '';
+    for (let i = 0; i < length; i++) {
+      tempPassword += chars.charAt(Math.floor(Math.random() * chars.length));
+    }
+    return tempPassword;
+  }
